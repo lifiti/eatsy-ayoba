72592311a03fbc63ef93fbdf4dd229604e7ab238
vutivi <vutivi@mobilemacs.co.za>
2020-04-04 18:56:08 +0200

    chore: redirect to firebase

--- server.js
+++ server.js
@@ -5,39 +5,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Set public folder as root
-app.use(express.static('public'));
+// // Set public folder as root
+// app.use(express.static('public'));
 
-// Parse POST data as URL encoded data
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+// // Parse POST data as URL encoded data
+// app.use(bodyParser.urlencoded({
+//   extended: true,
+// }));
 
-// Parse POST data as JSON
-app.use(bodyParser.json());
+// // Parse POST data as JSON
+// app.use(bodyParser.json());
 
-// Provide access to node_modules folder
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+// // Provide access to node_modules folder
+// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 
-const errorHandler = (err, req, res) => {
-  if (err.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    res.status(403).send({ title: 'Server responded with an error', message: err.message });
-  } else if (err.request) {
-    // The request was made but no response was received
-    res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    res.status(500).send({ title: 'An unexpected error occurred', message: err.message });
-  }
-};
+// const errorHandler = (err, req, res) => {
+//   if (err.response) {
+//     // The request was made and the server responded with a status code
+//     // that falls out of the range of 2xx
+//     res.status(403).send({ title: 'Server responded with an error', message: err.message });
+//   } else if (err.request) {
+//     // The request was made but no response was received
+//     res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
+//   } else {
+//     // Something happened in setting up the request that triggered an Error
+//     res.status(500).send({ title: 'An unexpected error occurred', message: err.message });
+//   }
+// };
 
-// Redirect all traffic to index.html
-app.use((req, res) => res.sendFile(`${__dirname}/index.html`));
+// // Redirect all traffic to index.html
+// app.use((req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('listening on %d', port);
 });
+app.get("/", (req, res) => {
+
+  res.status(301).redirect("https://eatsy-ce960.firebaseapp.com/")
+
+})
